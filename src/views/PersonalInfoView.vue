eb9159d37ca74271d378fe8d81ceab1caa23a977
Felipe Aguiar <felipe.pimentel.aguiar@gmail.com>
2024-10-10 16:12:40 -0500

    add faker

---
+++ src/views/PersonalInfoView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { faker } from '@faker-js/faker';
+
+const personalInfo = {
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+}
+</script>
+
+<template>
+  <div>{{ personalInfo.firstName }}</div>
+  <div>{{ personalInfo.lastName }}</div>
+</template>
